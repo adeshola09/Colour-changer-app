44d68d0f973c74707d81fad355041f5de90eca15
Adeshola09 <laminaidris1@gmail.com>
2022-08-14 23:53:20 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,17 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export const App = () => {
+  const [Color, setColor] = useState("");
+  const [Change, setChange] = useState();
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="color-background" style={{ boxShadow: `10px 10px 50px 20px ${Color}`, backgroundColor: Color }}></div>
+      <input type="text" placeholder="Type your color" onChange={(e) => setChange(e.target.value)} />
+      <button onClick={() => setColor(Change)}>change</button>
     </div>
   );
-}
+};
 
 export default App;
